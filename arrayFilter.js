a3491cad9295a1d9efa8045eb297bb4ba6d12e8e
Josh Bailey <joshuaclaytonbailey@gmail.com>
2020-03-04 21:35:29 -0800

    initial commit

---
+++ arrayFilter.js
@@ -0,0 +1,8 @@
+const Animals = [
+    { name: "Fluffykins", species: "rabbit" },
+    { name: "Caro", species: "dog" },
+    { name: "Hamilton", species: "dog" },
+    { name: "Harold", species: "fish" },
+    { name: "Ursula", species: "cat" },
+    { name: "Jimmy", species: "fish" }
+]
